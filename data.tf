b7927dafbef26b807645ef10bdf86731932d31ca
TkM Developer <tushar.mishra1165@gmail.com>
2024-09-01 10:10:03 +0530

    Removed unreferenced code and User Data script

--- data.tf
+++ data.tf
@@ -5,19 +5,3 @@ data "http" "myip" {
 data "aws_ssm_parameter" "jenkins_ami" {
   name = "/ami/Ubuntu/Jenkins"
 }
-
-data "aws_ami" "amazon_linux_3" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20240801"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
